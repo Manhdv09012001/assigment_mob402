dd90b824ecbc2011fe1ff7bc2955b5ad7818aa73
= <Manhdvph10986@fpt.edu.vn>
2021-04-22 14:47:34 +0700

    assigment

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect,useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import Home from "./screens/homeScreen";
+import Update from "./screens/updateScreen";
+
 
 export default function App() {
+  const Stack = createStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={styles.container}>
+      <NavigationContainer>
+
+          <Stack.Navigator>
+              <Stack.Screen
+                  name="Home"
+                  component={Home}
+                  options={{title:"Danh SACH"}}
+              />
+
+              <Stack.Screen
+                  name="Update"
+                  component={Update}
+                  options={{title:"CAP NHAP"}}
+              />
+
+            
+          </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
